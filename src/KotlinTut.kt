f2010554151065790c2db79c3806fd58922dd6a9
Miguel Saenz <msaenz424@gmail.com>
2017-12-05 21:36:25 -0800

    Strings

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -13,4 +13,16 @@ fun main(args: Array<String>){
     println("3.14 to Int: " + (3.14.toInt()))
     println("A to Int: " + ('A').toInt())
     println("65 to Char: " + 65.toChar())
+
+    /** Strings */
+    val longStr = "this a long long string just for test"
+    println("long string size: ${longStr.length}")
+    var str1 = "A random string"
+    var str2 = "a random string"
+    println("String equals: ${str1.equals(str2)}" )
+    println("Compare A to A: ${"A".compareTo("A")}")
+    println("Compare A to B: ${"A".compareTo("B")}")
+    println("2nd Index of str1: ${str1[2]}")
+    println("Index 2 - 7: ${str1.subSequence(2,7)}")
+    println("Contains random: ${str1.contains("random")}")
 }
