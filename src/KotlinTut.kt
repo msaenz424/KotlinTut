2f44d2b3e4e6b9365f518fea7fad6ee44323b4b1
Miguel Saenz <msaenz424@gmail.com>
2017-12-05 21:19:53 -0800

    Casting

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -9,4 +9,8 @@ fun main(args: Array<String>){
     var letterGrade: Char = 'A'
     println("A is a Char: ${letterGrade is Char}")
 
+    /** Casting */
+    println("3.14 to Int: " + (3.14.toInt()))
+    println("A to Int: " + ('A').toInt())
+    println("65 to Char: " + 65.toChar())
 }
