663e7ce613c03685c1cfc8e16c2c33ba0db04e4d
Miguel Saenz <msaenz424@gmail.com>
2017-12-07 18:04:45 -0800

    Loops

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -1,3 +1,4 @@
+import java.util.*
 
 fun main(args: Array<String>){
 
@@ -62,5 +63,39 @@ fun main(args: Array<String>){
             println("Go to Grade $grade")
         }
         else -> println("Go to College")
+    }
+
+    /** Loops */
+    for(x in 1..10){
+        println("Loop: $x")
+    }
+
+    val rand = Random()
+    val magicNum = rand.nextInt(50) + 1
+
+    var guess = 0
+
+    while (magicNum != guess){
+        guess += 1
+    }
+
+    println("Magic number was $guess")
+
+    for (x in 1..20){
+        if (x % 2 == 0){
+            continue        // continue jumps to the step in loop
+        }
+        println("Odd: $x")
+        if (x == 15) break  // break exits the loop
+    }
+
+    var arr3: Array<Int> = arrayOf(3, 6, 9)
+
+    for (i in arr3.indices){
+        println("Mult 3: ${arr3[i]}")
+    }
+
+    for ((index, value) in arr3.withIndex()){
+        println("Index: $index - Value: $value")
     }
 }
